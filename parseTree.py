42858760b2b265772144be85a7934c951d0da9fe
AhmedElhefnawy <ahmedelhefnawy2003@yahoo.com>
2023-11-22 20:14:48 +0200

    Parse Tree Added

---
+++ parseTree.py
@@ -0,0 +1,33 @@
+from stack import stack
+from Tree import binaryTree
+
+
+def buildParseTree(fpExp):
+
+    fpList = fpExp.split()
+    pStack = stack()
+    etree = binaryTree('')
+    pStack.push(etree)
+    currentTree = etree
+
+    for i in fpList:
+
+        if i == '(':
+            currentTree.insertLeft('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getLeftChild()
+
+        elif i not in '+=*/':  # Operand
+            currentTree.setRootVal(int(i))
+            currentTree = pStack.pop()
+
+        elif i in '+=*/':  # Operator
+            currentTree.setRootVal(i)
+            currentTree.insertRight('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getRightChild()
+
+        else:
+            print("Error: I Don't recognize" + i)
+
+    return etree
